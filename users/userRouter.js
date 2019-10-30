436a500ee012fb20bc5206bc73ee4427cab088f3
Uzias Rivera <uziasrivera@gmail.com>
2019-10-30 15:15:52 -0700

    fleshed out validateUser

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const userDB = require('./userDb.js')
 
-router.post('/', (req, res) => {
-
+router.post('/users', validateUser,(req, res) => {
+    userDB.insert(req.body)
+    .then(user=>{res.status(200).json(user)})
+    .catch(err=>{res.status(500).json({error:'could not be created'})})
 });
 
 router.post('/users/:id/posts', (req, res) => {
@@ -26,7 +28,6 @@ router.get('/users/:id', (req, res) => {
     userDB.getById(id)
     .then(users=>res.status(200).json(users))
     .catch(err=>res.status(500).json({error: 'something went wrong'}))
-
 });
 
 router.get('/users/:id/posts', (req, res) => {
@@ -54,6 +55,7 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
     //validates body on a request to create a new user
+    ("name" in req.body)? next():res.status(400).json({error:"You need a name to create a user"})
 
 };
 
