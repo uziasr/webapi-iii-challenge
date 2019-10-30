bc8da1370e63063b428a174e24fe41a83d4e2423
Uzias Rivera <uziasrivera@gmail.com>
2019-10-30 13:32:50 -0700

    completed get request for postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,20 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const postDB = require('./postDb');
 
+router.get('/posts', (req, res) => {
+    postDB.get()
+    .then(post=>{res.status(200).json(post)})
+    .catch(err=>{res.status(500).json({err:'this did not go well'})})
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/posts/:id', (req, res) => {
+    const id = req.params.id
+    postDB.getById(id)
+    .then(post=>{res.status(201).json(post)})
+    .catch(err=>{res.status(500).json({err:'this did not go well'})})
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+const postRouter = require('./posts/postRouter')
 
-server.get('/', (req, res) => {
+server.get('', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+// custom middleware
 
 function logger(req, res, next) {
+  console.log( new Date().toISOString())
+    console.log('HTTP METHOD: ',req.method,' URL: ',req.originalUrl)
+    next();
 
 };
+server.use(logger)
+server.use('/', postRouter)
+
+
 
 module.exports = server;
